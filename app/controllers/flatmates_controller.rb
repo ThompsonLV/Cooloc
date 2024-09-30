50c9c681503e435f8db9ac9e7bbf7b1e8cdee65a
Thomas Le Veo <thomasleveo@gmail.com>
2024-09-30 22:01:42 +0200

    Creation index et new

--- app/controllers/flatmates_controller.rb
+++ app/controllers/flatmates_controller.rb
@@ -1,7 +1,20 @@
 class FlatmatesController < ApplicationController
 
-def index
-    @flatmates = Flatmate.all
-end
+  def index
+      @flatmates = Flatmate.all
+  end
+
+  def show
+    @flatmate = Flatmate.find(params[:id])
+  end
+
+  def new
+    @flatmate = Flatmate.new
+  end
+
+  private
+  def flatmate_params
+    params.require(:flatmate).permit(:first_name, :last_name, :birthday, :description, :colocation_id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "flatmates#index"
-  get "about", to: "pages#about"
-  get "flatmates", to: "flatmates#index"
+
+  resources :flatmates, only: [:index, :show, :new, :create]
 end
